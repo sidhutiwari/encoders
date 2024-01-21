2b1387f706e66e31254e8676c8221d1ab30bb50b
sidhu <sidhu@sidhus-MacBook-Pro-2.local>
2024-01-21 21:44:40 +0530

    first commit

---
+++ src/main/java/com/test/TestP.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestP {
+	
+	
+	@GetMapping("/abc")
+	String getValue()
+	{
+		return "test first apil";
+	}
+
+}
